1433c12817c26464a63913f63e2ecffb67d7e313
Angel Guerrero <ajguerreror@gmail.com>
2023-02-24 18:05:53 -0400

    Hello world and datatypes

---
+++ hello.py
@@ -0,0 +1,20 @@
+# Comentario Hola Mundo
+"""
+Esto es un comentario
+En varias lineas
+"""
+print('Hola mundo')
+print('Hola de nuevo')
+
+
+'''
+Esto es otro
+comentario en
+varias lineas
+'''
+# consultar el tipo de dato
+print(type("Hola")) # str
+print(type(5)) # int
+print(type(1.5)) # float
+print(type(1 + 2j)) # complex
+print(type(True)) # bool
